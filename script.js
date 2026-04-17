6a28c99599e33386bae28863e0c779381aa4f940
Sesadesigns <olesyakipriyanova@outlook.com>
2026-04-17 11:52:19 -0400

    new updates from the code of march 4

--- script.js
+++ script.js
@@ -5,13 +5,12 @@ const artists = document.querySelectorAll(".artist");
 const artworks = document.querySelectorAll(".artwork"); 
 const allDetails = document.querySelectorAll(".artist details");
     
-//-- waits navigations to be clicked--//
     navLinks.forEach(link => { 
         link.addEventListener("click", e => { 
             e.preventDefault();
-//--reads which movement nav is beign clicked--//
-    const selectedMovement = link.dataset.movements.toLowerCase();
-//--shows and hides artworks depending on the movement nav--//       
+
+    const selectedMovement = (link.dataset.movements || "").toLowerCase();
+       
     artworks.forEach(img => { 
         const movements = img.dataset.movement 
             .toLowerCase() 
@@ -19,18 +18,20 @@ const allDetails = document.querySelectorAll(".artist details");
             
         img.style.display = 
         movements.includes(selectedMovement) ? "block" : "none"; });
-//-- shows and hides artists depending on the movement nav--//        
-        artists.forEach(artist => { const movements = artist.dataset.movement .toLowerCase() .split(" ");
+        
+        artists.forEach(artist => { const movements = artist.dataset.movement 
+            .toLowerCase() 
+            .split(" ");
             if (movements.includes(selectedMovement)) { 
                 artist.style.display = "block"; 
             } else { 
                 artist.style.display = "none";
             } 
         });
-//--reads which discipline nav is beign clicked--//
+
             const selectedDiscipline = link.dataset.disciplines.toLowerCase();
                 
- //--shows and hides artworks depending on the discipline nav--//        
+        
             artworks.forEach(img => { 
                 const disciplines = img.dataset.discipline 
                 .toLowerCase() 
@@ -39,8 +40,7 @@ const allDetails = document.querySelectorAll(".artist details");
             img.style.display = disciplines.includes(selectedDiscipline) 
                 ? "block" : "none"; 
             });
- //-- shows and hides artists depending on the discipline nav--//        
-           
+            
             artists.forEach(artist => {        
                 const movements = artist.dataset.movement 
                 .toLowerCase() .split(" ");
@@ -51,13 +51,12 @@ const allDetails = document.querySelectorAll(".artist details");
                     artist.style.display = "none"; 
                 } 
                 });
-//--closes items when new one is clicked--//
+
             allDetails.forEach(details => { 
                 details.open = false; 
             }); 
         }); 
     });
-//--only one artist biois open at a time--//
             allDetails.forEach(details => { 
                 details.addEventListener("toggle", () => { 
                     if (details.open) { 
